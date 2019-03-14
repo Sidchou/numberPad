fea2517e58c689cc6b00e0ee7ee0ee0556fa561a
Sid <sidchou93@gmail.com>
2019-03-14 16:50:36 -0400

    Initial Commit

---
+++ numberPadTests/numberPadTests.swift
@@ -0,0 +1,34 @@
+//
+//  numberPadTests.swift
+//  numberPadTests
+//
+//  Created by ching Hsi chou on 3/14/19.
+//  Copyright © 2019 ching Hsi chou. All rights reserved.
+//
+
+import XCTest
+@testable import numberPad
+
+class numberPadTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
